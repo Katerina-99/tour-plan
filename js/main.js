d6b0bfdba4501ffe5bbd0679577f2880de931284
Katerina-99 <kivi80x@gmail.com>
2020-09-26 21:23:55 -0200

    create: yandex map API & fixed responsive for map

--- js/main.js
+++ js/main.js
@@ -22,6 +22,25 @@ var reviewsSlider = new Swiper(".reviews-slider", {
   navigation: {
     nextEl: ".reviews-slider__button--next",
     prevEl: ".reviews-slider__button--prev"
-  },
-  
+  }
 });
+
+ymaps.ready(init);
+var myMap,
+  myPlacemark;
+function init() {
+   myMap = new ymaps.Map("map", {
+     center: [7.838190710155044, 98.29882109172051],
+    zoom: 16
+  });
+
+  myPlacemark = new ymaps.Placemark([7.838190710155044, 98.29882109172051], {
+    hintContent: 'Phuket',
+    balloonContent: 'Hilton Hotel'
+  });
+
+  myMap.geoObjects
+    .add(myPlacemark);
+}
+
+
